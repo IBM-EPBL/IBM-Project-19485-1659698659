e31e552c7d5b25b509cfcffdf5bde50ff202b702
Sayad Pervez <pervez2504@gmail.com>
2022-10-29 20:43:36 +0530

    No camera commit

--- Project Development Phase/Sprint 1/main.py
+++ Project Development Phase/Sprint 1/main.py
@@ -1,8 +1,55 @@
 # Python code
 
+# IMPORT SECTION STARTS
+
 import weather
+from datetime import datetime as dt
+
+# IMPORT SECTION ENDS
+# -----------------------------------------------
+# USER INPUT SECTION STARTS
 
 myLocation = "Chennai,IN"
 APIKEY = "bf4a8d480ee05c00952bf65b78ae826b"
 
-locationData = weather.get(myLocation,APIKEY)
+localityInfo = {
+    "schools" : {
+        "schoolZone" : True,
+        "activeTime" : ["7:00","17:30"] # schools active from 7 AM till 5:30 PM
+        },
+    "hospitalsNearby" : False,
+    "usualSpeedLimit" : 40 # in km/hr
+}
+
+# USER INPUT SECTION ENDS
+# -----------------------------------------------
+# APP LOGIC SECTION STARTS
+
+weatherData = weather.get(myLocation,APIKEY)
+
+print(weatherData)
+
+finalSpeed = localityInfo["usualSpeedLimit"] if "rain" not in weatherData else localityInfo["usualSpeedLimit"]/2
+finalSpeed = finalSpeed if weatherData["visibility"]>35 else finalSpeed/2
+
+if(localityInfo["hospitalsNearby"]):
+    # hospital zone
+    doNotHonk = True
+else:
+    if(localityInfo["schools"]["schoolZone"]==False):
+        # neither school nor hospital zone
+        doNotHonk = False
+    else:
+        # school zone
+        now = [dt.now().hour,dt.now().minute]
+        activeTime = [list(map(int,_.split(":"))) for _ in localityInfo["schools"]["activeTime"]]
+        doNotHonk = activeTime[0][0]<=now[0]<=activeTime[1][0] and activeTime[0][1]<=now[1]<=activeTime[1][1]
+
+outputObject = {
+     "speed" : finalSpeed,
+     "doNotHonk" : doNotHonk
+}
+
+print(outputObject)
+
+# APP LOGIC SECTION ENDS
